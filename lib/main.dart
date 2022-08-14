35ed59c2b022986698f1bac9b5aa34c3affee342
Barzy-Yasin <barzy.nettwork@gmail.com>
2022-08-14 14:37:01 +0300

    firebase init

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:job_vacancies/src/root_app.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const RootApp());
 }
-

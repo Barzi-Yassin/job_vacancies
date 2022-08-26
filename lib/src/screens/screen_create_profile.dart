bc7172180173d3dd80abff7ecf0319c9b48bab8d
Barzy-Yasin <barzy.nettwork@gmail.com>
2022-08-27 02:04:17 +0300

    image picker ui is ready in create profile screen

--- lib/src/screens/screen_create_profile.dart
+++ lib/src/screens/screen_create_profile.dart
@@ -50,183 +50,198 @@ class _ScreenCreateProfileState extends State<ScreenCreateProfile> {
     return Scaffold(
       backgroundColor: Colors.grey,
       body: SafeArea(
-          child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Text('${widget.user}'),
-              ),
-              // Text('\nuser.uid: ${widget.user?.uid}'),
-              const SizedBox(height: 20),
-              // name
-              const Padding(
-                padding: EdgeInsets.fromLTRB(0, 0, 240, 0),
-                child: Text(
-                  'Name',
-                  style: TextStyle(fontSize: 16),
+        child: ListView(
+          // mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text('${widget.user}'),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.fromLTRB(30, 10, 30, 10),
-                child: SizedBox(
-                  height: 50,
-                  child: TextField(
-                    controller: controllerName,
-                    obscureText: false,
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(10)),
-                      filled: true,
+                // Text('\nuser.uid: ${widget.user?.uid}'),
+                const SizedBox(height: 20),
+                // name
+                const Padding(
+                  padding: EdgeInsets.fromLTRB(0, 0, 240, 0),
+                  child: Text(
+                    'Name',
+                    style: TextStyle(fontSize: 16),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(30, 10, 30, 10),
+                  child: SizedBox(
+                    height: 50,
+                    child: TextField(
+                      controller: controllerName,
+                      obscureText: false,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10)),
+                        filled: true,
+                      ),
                     ),
                   ),
+                )
+              ],
+            ),
+            const SizedBox(height: 8),
+            // phone
+            Column(
+              children: [
+                const Padding(
+                  padding: EdgeInsets.fromLTRB(0, 0, 240, 0),
+                  child: Text(
+                    'Phone',
+                    style: TextStyle(fontSize: 16),
+                  ),
                 ),
-              )
-            ],
-          ),
-          const SizedBox(height: 8),
-          // phone
-          Column(
-            children: [
-              const Padding(
-                padding: EdgeInsets.fromLTRB(0, 0, 240, 0),
-                child: Text(
-                  'Phone',
-                  style: TextStyle(fontSize: 16),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.fromLTRB(30, 10, 30, 10),
-                child: SizedBox(
-                  height: 50,
-                  child: TextField(
-                    controller: controllerPhone,
-                    obscureText: false,
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(10)),
-                      filled: true,
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(30, 10, 30, 10),
+                  child: SizedBox(
+                    height: 50,
+                    child: TextField(
+                      controller: controllerPhone,
+                      obscureText: false,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10)),
+                        filled: true,
+                      ),
                     ),
                   ),
-                ),
-              )
-            ],
-          ),
-          const SizedBox(height: 8),
-          Column(
-            children: [
-              const Padding(
-                padding: EdgeInsets.fromLTRB(0, 0, 255, 0),
-                child: Text(
-                  'City',
-                  style: TextStyle(fontSize: 16),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.fromLTRB(30, 10, 30, 10),
-                child: SizedBox(
-                  height: 50,
-                  child: CustomDropdownButton2(
-                    buttonWidth: 300,
-                    hint: 'select city',
-                    buttonDecoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        border: Border.all(
-                            color: const Color.fromARGB(255, 124, 124, 124)),
-                        color: const Color.fromARGB(255, 237, 237, 237)),
-                    dropdownItems: cities,
-                    value: selectedCity,
-                    onChanged: (value) {
-                      setState(() {
-                        selectedCity = value;
-                        debugPrint(selectedCity!);
-                      });
-                    },
+                )
+              ],
+            ),
+            const SizedBox(height: 8),
+            Column(
+              children: [
+                const Padding(
+                  padding: EdgeInsets.fromLTRB(0, 0, 255, 0),
+                  child: Text(
+                    'City',
+                    style: TextStyle(fontSize: 16),
                   ),
                 ),
-              )
-            ],
-          ),
-          const SizedBox(
-            height: 8,
-          ),
-          Column(
-            children: [
-              const Padding(
-                padding: EdgeInsets.fromLTRB(20, 0, 220, 0),
-                child: Text(
-                  'Job Category',
-                  style: TextStyle(fontSize: 16),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.fromLTRB(30, 10, 30, 10),
-                child: SizedBox(
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(30, 10, 30, 10),
+                  child: SizedBox(
                     height: 50,
                     child: CustomDropdownButton2(
                       buttonWidth: 300,
-                      hint: 'select job',
+                      hint: 'select city',
                       buttonDecoration: BoxDecoration(
                           borderRadius: BorderRadius.circular(10),
                           border: Border.all(
                               color: const Color.fromARGB(255, 124, 124, 124)),
                           color: const Color.fromARGB(255, 237, 237, 237)),
-                      dropdownItems: jobs,
-                      value: selectedJob,
+                      dropdownItems: cities,
+                      value: selectedCity,
                       onChanged: (value) {
                         setState(() {
-                          selectedJob = value;
-                          debugPrint(selectedJob!);
+                          selectedCity = value;
+                          debugPrint(selectedCity!);
                         });
                       },
-                    )),
-              )
-            ],
-          ),
-          // const RadioGroup(),
-
-          // create button
-          Padding(
-            padding: const EdgeInsets.fromLTRB(200, 30, 0, 0),
-            child: ElevatedButton(
-              onPressed: () async {
-                // "name": controllerName.text,
-                // "phone": controllerPhone.text,
-                // "city": selectedCity,
-                // "job_category": selectedJob,
-                // "email": widget.user!.email,
-                // "uid": widget.user!.uid,
-
-                if (widget.user != null) {
-                  JobUserModel theUser = JobUserModel(
-                    name: controllerName.text,
-                    phoneNumber: controllerPhone.text,
-                    jobCategory: selectedJob!, // it won't be null.
-                    city: selectedCity ??
-                        cities[
-                            0], // if it was null the first object in the cities list.
-                    email: widget.user!
-                        .email!, // it won't be null, bcz user registers using the email and password.
-                    uid: widget.user!.uid,
-                    // imageUrl:
-                  );
-
-                  // TODO: fix bug here important
-                  await FirebaseFirestore.instance
-                      .collection('users')
-                      .doc(widget.user!.uid)
-                      .set(theUser.toMap())
-                      .catchError((err) => debugPrint(err))
-                      .then((value) => debugPrint('done'));
-                }
-              },
-              child: const Text('Create Pprofile'),
+                    ),
+                  ),
+                )
+              ],
             ),
-          ),
-        ],
-      )),
+            const SizedBox(
+              height: 8,
+            ),
+            Column(
+              children: [
+                const Padding(
+                  padding: EdgeInsets.fromLTRB(20, 0, 220, 0),
+                  child: Text(
+                    'Job Category',
+                    style: TextStyle(fontSize: 16),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(30, 10, 30, 10),
+                  child: SizedBox(
+                      height: 50,
+                      child: CustomDropdownButton2(
+                        buttonWidth: 300,
+                        hint: 'select job',
+                        buttonDecoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            border: Border.all(
+                                color:
+                                    const Color.fromARGB(255, 124, 124, 124)),
+                            color: const Color.fromARGB(255, 237, 237, 237)),
+                        dropdownItems: jobs,
+                        value: selectedJob,
+                        onChanged: (value) {
+                          setState(() {
+                            selectedJob = value;
+                            debugPrint(selectedJob!);
+                          });
+                        },
+                      )),
+                )
+              ],
+            ),
+            // const RadioGroup(),
+
+            // image picker 
+            Container(
+              padding: EdgeInsets.all(40),
+              child: Column(
+                children: [
+                  Placeholder(),
+                  ElevatedButton(onPressed: () {}, child: Text('Select Image')),
+                  ElevatedButton(
+                      onPressed: () {}, child: Text('Upload The Image')),
+                ],
+              ),
+            ),
+
+            // create profile button
+            Padding(
+              padding: const EdgeInsets.fromLTRB(200, 30, 30, 0),
+              child: ElevatedButton(
+                onPressed: () async {
+                  // "name": controllerName.text,
+                  // "phone": controllerPhone.text,
+                  // "city": selectedCity,
+                  // "job_category": selectedJob,
+                  // "email": widget.user!.email,
+                  // "uid": widget.user!.uid,
+
+                  if (widget.user != null) {
+                    JobUserModel theUser = JobUserModel(
+                      name: controllerName.text,
+                      phoneNumber: controllerPhone.text,
+                      jobCategory: selectedJob!, // it won't be null.
+                      city: selectedCity ??
+                          cities[
+                              0], // if it was null the first object in the cities list.
+                      email: widget.user!
+                          .email!, // it won't be null, bcz user registers using the email and password.
+                      uid: widget.user!.uid,
+                      // imageUrl:
+                    );
+
+                    // TODO: fix bug here important
+                    await FirebaseFirestore.instance
+                        .collection('users')
+                        .doc(widget.user!.uid)
+                        .set(theUser.toMap())
+                        .catchError((err) => debugPrint(err))
+                        .then((value) => debugPrint('done'));
+                  }
+                },
+                child: const Text('Create Pprofile'),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
